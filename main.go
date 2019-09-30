f61a62af186ea5017f03b0a4df4981bb2cc0b436
aphiratnimanussonkul <b5923151@gmail.com>
2019-09-30 23:31:22 +0700

    add models

---
+++ main.go
@@ -0,0 +1,16 @@
+package go_websocket
+
+import "github.com/aphiratnimanussonkul/go-websocket/models"
+
+var manager = models.ClientManager{
+	broadcast:  make(chan []byte),
+	register:   make(chan *Client),
+	unregister: make(chan *Client),
+	clients:    make(map[*Client]bool),
+}
+func main() {
+	fmt.Println("Starting application...")
+	go manager.start()
+	http.HandleFunc("/ws", wsPage)
+	http.ListenAndServe(":12345", nil)
+}
